3e0b362342e36484efed19b5de824510b34dbfc8
Frank Faustino <nawzyah@gmail.com>
2019-02-19 00:46:02 -0800

    ✨ Add nuke functionality

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 const fs = require('fs')
-const { join } = require('path')
 const { homedir } = require('os')
+const { join } = require('path')
+const readline = require('readline')
 const { promisify } = require('util')
 
-const readdir = promisify(fs.readdir)
 const lstat = promisify(fs.lstat)
+const readdir = promisify(fs.readdir)
 const path = join(homedir(), '/Library/Caches/com.spotify.client/Data')
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
 const main = async () => {
-  let total = 0
+  const output = {
+    files: [],
+    total: 0
+  }
   const stack = [path]
 
   while (stack.length) {
     const current = stack.pop()
-
     const stats = await lstat(current)
 
-    if (stats.isFile()) total += stats.size
+    if (stats.isFile()) {
+      output.files.push(current)
+      output.total += stats.size
+    }
 
     if (stats.isDirectory()) {
       const dir = await readdir(current)
@@ -24,7 +35,34 @@ const main = async () => {
       dir.forEach(dirItem => stack.push(join(current, dirItem)))
     }
   }
-  return total
+
+  return output
 }
 
-main().then(total => console.log(total))
+const nukeCache = files => {
+  files.forEach(async file => {
+    console.log(`💥 ${file}`)
+    await fs.unlinkSync(file)
+  })
+
+  console.log('ALL DONE. KTHXBYE 👋')
+  rl.close()
+}
+
+main()
+  .then(({ files, total }) => {
+    if (files.length < 1) {
+      console.log('Spotify cache is empty. Nothing to nuke. 🎉')
+      rl.close()
+    } else {
+      console.log(`Spotify cache has ${files.length} files and is ${(total / 1e9).toFixed(2)} GB 😱`)
+
+      rl.question('Would you like to nuke it? y / n:  ', answer => {
+        if (answer === 'y') {
+          console.log('\nOK. NUKING...')
+          nukeCache(files)
+        }
+      })
+    }
+  })
+  .catch(err => console.error(err))
